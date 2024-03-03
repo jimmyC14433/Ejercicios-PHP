e9528879ae323442b664c5c1bd0a0f3e39c7f5d0
JimmyC14433 <114953025+jimmyC14433@users.noreply.github.com>
2024-03-02 19:34:43 -0500

    Add files via upload

---
+++ pruebas.php
@@ -0,0 +1,13 @@
+<?php 
+/*
+$num = 17591026060781;
+$num1 = 1;
+$num1 = 2;
+*/
+$número_grande1 = 17591026060781+1;
+$número_grande2 = 17591026060781/2;
+$número_grande = $número_grande2*$número_grande1;
+/* var_dump($número_grande); */  
+echo $número_grande;     
+
+?>
